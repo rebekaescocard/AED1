d88ef07896afa93f62174f0a61ce77d61ac9d6d5
rebekaescocard <rebeka.escocard@unifesp.br>
2025-11-30 21:18:35 -0300

    1110.c

--- 1110.c
+++ 1110.c
@@ -7,23 +7,19 @@ int main() {
         int queue[60];
         int front = 0, rear = n - 1;
 
-        // inicializa a fila de 1 a n
         for (int i = 0; i < n; i++)
             queue[i] = i + 1;
 
         printf("Discarded cards:");
 
-        // enquanto houver mais de 1 carta
         while (front != rear) {
 
-            // descarta a carta do topo
             printf(" %d", queue[front]);
             front = (front + 1) % n;
 
             if (front != rear)
                 printf(",");
 
-            // move a próxima carta para o final
             rear = (rear + 1) % n;
             queue[rear] = queue[front];
             front = (front + 1) % n;
@@ -33,4 +29,5 @@ int main() {
     }
 
     return 0;
+
 }
