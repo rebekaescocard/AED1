98ed834828f9944b4c4be9e8bf555b17262ba5bb
rebekaescocard <rebeka.escocard@unifesp.br>
2026-01-07 20:16:33 -0300

    1068.c

--- 1068.c
+++ 1068.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SIZE 1001
-
 int main() {
-    char expressao[MAX_SIZE]; 
+    char expressao[1001]; 
     
     while (scanf("%s", expressao) != EOF) {
-        
         int balanco = 0;
-        int correto = 1; 
+        int erro = 0; 
 
         for (int i = 0; i < strlen(expressao); i++) {
             char c = expressao[i];
 
             if (c == '(') {
                 balanco++;
-            } else if (c == ')') {
-                if (balanco > 0) {
-                    balanco--;
-                } else {
-                    correto = 0;
+            } 
+            
+            if (c == ')') {
+                balanco--;
+                if (balanco < 0) {
+                    erro = 1;
                     break; 
                 }
             }
         }
 
-        if (correto && balanco == 0) {
+        if (erro == 0 && balanco == 0) {
             printf("correto\n");
-        } else {
+        } 
+        else {
             printf("incorreto\n");
         }
     }
     
     return 0;
+
 }
