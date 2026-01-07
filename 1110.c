8bdef0ceec986e939a57b6c624a47d3bef434539
rebekaescocard <rebeka.escocard@unifesp.br>
2026-01-07 20:27:15 -0300

    1110.c

--- 1110.c
+++ 1110.c
@@ -4,30 +4,33 @@ int main() {
     int n;
 
     while (scanf("%d", &n) && n != 0) {
-        int queue[60];
-        int front = 0, rear = n - 1;
+        int enfilera[60];
+        int frente = 0, fim = n - 1;
 
-        for (int i = 0; i < n; i++)
-            queue[i] = i + 1;
-
-        printf("Discarded cards:");
-
-        while (front != rear) {
-
-            printf(" %d", queue[front]);
-            front = (front + 1) % n;
-
-            if (front != rear)
-                printf(",");
-
-            rear = (rear + 1) % n;
-            queue[rear] = queue[front];
-            front = (front + 1) % n;
+        for (int i = 0; i < n; i++){
+            enfilera[i] = i + 1;
         }
 
-        printf("\nRemaining card: %d\n", queue[front]);
+        printf("cartas descartadas:");
+
+        while (frente != fim) {
+
+            printf(" %d", enfilera[frente]);
+            frente = (frente + 1) % n;
+
+            if (frente != fim){
+                printf(",");
+            }
+
+            fim = (fim + 1) % n;
+            enfilera[fim] = enfilera[frente];
+            frente = (frente + 1) % n;
+        }
+
+        printf("\ncartas restantes: %d\n", enfilera[frente]);
     }
 
     return 0;
 
 }
+
