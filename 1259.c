c1dc8e899b32804d9ffe1e9719ddebdbab300e48
rebekaescocard <rebeka.escocard@unifesp.br>
2026-01-08 16:25:50 -0300

    1259.c

--- 1259.c
+++ 1259.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cmp_asc(const void *a, const void *b) {
+int compara_asc(const void *a, const void *b){
     return (*(int*)a - *(int*)b);
 }
 
-int cmp_desc(const void *a, const void *b) {
+int compara_desc(const void *a, const void *b){
     return (*(int*)b - *(int*)a);
 }
 
-int main() {
+int main(){
     int n;
     scanf("%d", &n);
 
     int pares[n], impares[n];
     int p = 0, i = 0;
 
-    for (int k = 0; k < n; k++) {
+    for (int k = 0; k < n; k++){
         int x;
         scanf("%d", &x);
 
-        if (x % 2 == 0)
+        if (x % 2 == 0){
             pares[p++] = x;
-        else
+        }
+        else{
             impares[i++] = x;
+        }
     }
 
-    qsort(pares, p, sizeof(int), cmp_asc);
+    qsort(pares, p, sizeof(int), compara_asc);
+    qsort(impares, i, sizeof(int), compara_desc);
 
-    qsort(impares, i, sizeof(int), cmp_desc);
-
-    for (int k = 0; k < p; k++)
+    for (int k = 0; k < p; k++){
         printf("%d\n", pares[k]);
+    }
 
-    for (int k = 0; k < i; k++)
+    for (int k = 0; k < i; k++){
         printf("%d\n", impares[k]);
-
+    }
     return 0;
 }
