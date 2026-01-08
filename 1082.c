342005f792a4fd953af3c078349f94afa68d2274
rebekaescocard <rebeka.escocard@unifesp.br>
2026-01-08 13:58:01 -0300

    1082.c

--- 1082.c
+++ 1082.c
@@ -1,70 +1,68 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX 30
+int mat[26][26];
+int visitado[26];
+char componente[27];
+int cont_componente;
 
-int N, M;
-int adj[MAX][MAX];
-int visited[MAX];
-char comp[MAX];
-int compSize;
+int compara(const void *a, const void *b){
+    return (*(char *)a - *(char *)b);
+}
 
-void dfs(int v) {
-    visited[v] = 1;
-    comp[compSize++] = 'a' + v;
-    for (int i = 0; i < N; i++) {
-        if (adj[v][i] && !visited[i]) {
-            dfs(i);
+void dfs(int u, int v){
+    visitado[u] = 1;
+    componente[cont_compoente++] = u + 'a';
+
+    for (int i = 0; i < v; i++){
+        if (mat[u][i] && !visitado[i]){
+            dfs(i, v);
         }
     }
 }
 
-int main() {
-    int T;
-    scanf("%d", &T);
+int main(){
+    int n, v, e, i, j, k, x, y;
+    char c1, c2;
 
-    for (int t = 1; t <= T; t++) {
-        scanf("%d %d", &N, &M);
+    if (scanf("%d", &n) == EOF){ 
+        return 0;
+    }
 
-        memset(adj, 0, sizeof(adj));
-        memset(visited, 0, sizeof(visited));
+    for (i = 1; i <= n; i++){
+        scanf("%d %d", &v, &e);
 
-        for (int i = 0; i < M; i++) {
-            char u, v;
-            scanf(" %c %c", &u, &v);
-            int a = u - 'a';
-            int b = v - 'a';
-            adj[a][b] = adj[b][a] = 1;
-        }
-
-        printf("Case #%d:\n", t);
-
-        int components = 0;
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                compSize = 0;
-                dfs(i);
-                // Ordena os vértices do componente
-                for (int x = 0; x < compSize; x++) {
-                    for (int y = x+1; y < compSize; y++) {
-                        if (comp[x] > comp[y]) {
-                            char tmp = comp[x];
-                            comp[x] = comp[y];
-                            comp[y] = tmp;
-                        }
-                    }
-                }
-                // Imprime o componente
-                for (int x = 0; x < compSize; x++) {
-                    printf("%c,", comp[x]);
-                }
-                printf("\n");
-                components++;
+        for (x = 0; x < 26; x++){
+            visitado[x] = 0; 
+            for (y = 0; y < 26; y++){
+                mat[x][y] = 0; 
             }
         }
 
-        printf("%d connected components\n\n", components);
-    }
+        for (j = 0; j < e; j++){
+            scanf(" %c %c", &c1, &c2);
+            mat[c1 - 'a'][c2 - 'a'] = 1;
+            mat[c2 - 'a'][c1 - 'a'] = 1;
+        }
 
+        printf("caso #%d:\n", i);
+        int numero_componentes = 0;
+
+        for (j = 0; j < v; j++){
+            if (!visitado[j]){
+                cont_componente = 0;
+                dfs(j, v);
+                
+                qsort(componente, cont_componente, sizeof(char), compara);
+                
+                for (k = 0; k < cont_componente; k++){
+                    printf("%c,", componente[k]);
+                }
+                printf("\n");
+                numero_componentes++;
+            }
+        }
+        printf("%d connected components\n\n", numero_componentes);
+    }
     return 0;
 }
