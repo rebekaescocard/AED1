ba110596a9ceaa066f55b5875970aba97a74af35
rebekaescocard <rebeka.escocard@unifesp.br>
2026-01-08 16:36:52 -0300

    2654.c

--- 2654.c
+++ 2654.c
@@ -7,30 +7,34 @@ typedef struct {
     int poder;
     int matou;
     int morreu;
-} Guerreiro;
+}guerreiro;
 
-int cmp(const void *a, const void *b) {
-    Guerreiro *x = (Guerreiro*)a;
-    Guerreiro *y = (Guerreiro*)b;
+int compara(const void *a, const void *b){
+    guerreiro *x = (guerreiro*)a;
+    guerreiro *y = (guerreiro*)b;
 
-    if (x->poder != y->poder) return y->poder - x->poder; 
-    if (x->matou != y->matou) return y->matou - x->matou; 
-    if (x->morreu != y->morreu) return x->morreu - y->morreu; 
+    if (x->poder != y->poder){
+        return y->poder - x->poder;
+    }
+    if (x->matou != y->matou){ 
+        return y->matou - x->matou;
+    }
+    if (x->morreu != y->morreu){ 
+        return x->morreu - y->morreu;
+    }
     return strcmp(x->nome, y->nome); 
 }
 
-int main() {
+int main(){
     int N;
     scanf("%d", &N);
 
-    Guerreiro guerreiros[N];
-    for (int i = 0; i < N; i++) {
-        scanf("%s %d %d %d", guerreiros[i].nome, &guerreiros[i].poder,
-              &guerreiros[i].matou, &guerreiros[i].morreu);
+    guerreiro guerreiros[N];
+    for (int i = 0; i < N; i++){
+        scanf("%s %d %d %d", guerreiros[i].nome, &guerreiros[i].poder,&guerreiros[i].matou, &guerreiros[i].morreu);
     }
 
-    qsort(guerreiros, N, sizeof(Guerreiro), cmp);
-
+    qsort(guerreiros, N, sizeof(guerreiro), compara);
     printf("%s\n", guerreiros[0].nome);
 
     return 0;
