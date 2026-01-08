263a42577ce7450ebb7812f7cd3ec13475cf1d4d
rebekaescocard <rebeka.escocard@unifesp.br>
2026-01-07 22:49:18 -0300

    1256.c

--- 1256.c
+++ 1256.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(){
     int T; 
     scanf("%d", &T);
 
-    for (int t = 0; t < T; t++) {
+    for (int t = 0; t < T; t++){
         int M, C;
         scanf("%d %d", &M, &C);
 
         int *hash[M];
-        int size[M];
-        for (int i = 0; i < M; i++) {
+        int tam[M];
+        for (int i = 0; i < M; i++){
             hash[i] = (int*)malloc(C * sizeof(int));
-            size[i] = 0;
+            tam[i] = 0;
         }
 
-        for (int i = 0; i < C; i++) {
+        for (int i = 0; i < C; i++){
             int x;
             scanf("%d", &x);
-            int pos = x % M;
-            hash[pos][size[pos]++] = x;
+            int posicao = x % M;
+            hash[posicao][tam[posicao]++] = x;
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++){
             printf("%d ->", i);
-            for (int j = 0; j < size[i]; j++) {
+            for (int j = 0; j < tam[i]; j++){
                 printf(" %d ->", hash[i][j]);
             }
             printf(" \\\n");
         }
 
-        if (t < T - 1) printf("\n");
+        if (t < T - 1){ 
+            printf("\n");
+        }
 
-        for (int i = 0; i < M; i++) free(hash[i]);
+        for (int i = 0; i < M; i++){ 
+            free(hash[i]);
+        }
     }
-
     return 0;
 }
