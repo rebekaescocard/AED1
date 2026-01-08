6427314c0dc85d421179b82b18b5e609f7e7a0b5
rebekaescocard <rebeka.escocard@unifesp.br>
2026-01-08 15:56:30 -0300

    2460.c

--- 2460.c
+++ 2460.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_ID 100001
 
-void resolve_problema() {
+void resolve_problema(){
     int N; 
-    if (scanf("%d", &N) != 1) return;
+    if (scanf("%d", &N) != 1){
+        return;
+    }
     int *fila_inicial = (int *)malloc(N * sizeof(int));
-    if (fila_inicial == NULL) exit(1);
+    if (fila_inicial == NULL){
+        exit(1);
+    }
 
-    for (int i = 0; i < N; i++) {
-        if (scanf("%d", &fila_inicial[i]) != 1) break;
+    for (int i = 0; i < N; i++){
+        if (scanf("%d", &fila_inicial[i]) != 1){ 
+            break;
+        }
     }
 
     int M; 
-    if (scanf("%d", &M) != 1) {
+    if (scanf("%d", &M) != 1){
         free(fila_inicial);
         return;
     }
 
-    int *saidas = (int *)calloc(MAX_ID, sizeof(int));
-    if (saidas == NULL) {
+    int *saidas = (int *)calloc(100001, sizeof(int));
+    if (saidas == NULL){
         free(fila_inicial);
         exit(1);
     }
 
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < M; i++){
         int id_saiu;
         if (scanf("%d", &id_saiu) != 1) break;
         
-        if (id_saiu < MAX_ID) {
+        if (id_saiu < 100001){
             saidas[id_saiu] = 1; 
         }
     }
 
     int primeira_impressao = 1; 
     
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++){
         int id_atual = fila_inicial[i];
-        if (id_atual < MAX_ID && saidas[id_atual] == 0) {
-            if (!primeira_impressao) {
+        if (id_atual < 100001 && saidas[id_atual] == 0){
+            if (!primeira_impressao){
                 printf(" ");
             }
             printf("%d", id_atual);
@@ -50,7 +55,7 @@ void resolve_problema() {
     free(saidas);
 }
 
-int main() {
+int main(){
     resolve_problema();
     return 0;
 }
